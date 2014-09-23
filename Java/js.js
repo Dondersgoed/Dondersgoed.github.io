5b5befa506eb25926af332cad61fec248821e884
Dondersgoed <dondersgoedvideografie@gmail.com>
2014-09-24 00:29:37 +0200

    Website Upload

---
+++ Java/js.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function ($) {
+
+
+    //initialise Stellar.js
+    $(window).stellar();
+
+    //Cache some variables
+    var links = $('.navigation').find('li');
+    slide = $('.slide');
+    button = $('.button');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+
+   
+
+
+});
